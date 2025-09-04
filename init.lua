455c9afc123373a2aee8484e8fd620cb8093db29
Jacob.Vanthof <jacob.vanthof@outlook.com>
2025-09-04 12:35:06 -0700

    Add init file

---
+++ init.lua
@@ -0,0 +1,5 @@
+local digimon = require('digi.nvim')
+
+vim.api.nvim_create_user_command('DigiSearch', function(opts)
+    digimon.search_by_card_number(opts.args)
+end, {nargs = 1, desc = "Search Digimon card by card number (e.g. BT4-016)"})
